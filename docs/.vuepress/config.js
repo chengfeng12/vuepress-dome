4620e16428a119efab85c4e324a0c71c490919d8
yxr <yxr@qq.com>
2020-12-07 10:37:15 +0800

    配置pwa，但未进行服务器配置，github pages 配置失败，所以 pwa 暂看不出来效果

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,15 @@ module.exports = {
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'author', content: '我是一只鱼' }],
     ['meta', { name: 'keywords', content: '我是一只鱼的博客网站，vuepress' }],
+
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   base: '/docs/',
   plugins: [
@@ -20,6 +29,16 @@ module.exports = {
           // lang
           moment.locale("zh-cn")
           return moment(timestamp).format("LLLL")
+        }
+      }
+    ],
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "新的公告",
+          buttonText: "更新提示"
         }
       }
     ]
@@ -97,7 +116,8 @@ module.exports = {
         'about',
         'page-a',
         ['page-a', 'pagea - 对象'],
-        ['page-b', 'pageb - 对象']
+        ['page-b', 'pageb - 对象'],
+        'PWA',
       ]
     }
     
---
+++ docs/PWA.md
@@ -0,0 +1,7 @@
+---
+sidebarDepth: 2
+---
+
+#### pwa 测试
+
+查看 pwa 是否成效 (需在项目中安装 vuepress)
